23d84fbb37b9f06e506caa4a75397b67f3fad1d0
NHOY <NHOY@2210DR000919>
2023-06-20 17:28:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp84/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp84
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string symbol;
+            int lengthName = 0;
+            int symbolEdgeName = 2;
+
+            Console.Write("Введите имя:");
+            name = Console.ReadLine();
+            Console.Write("Введите символ, который обведёт ваше имя в рамку:");
+            symbol = Console.ReadLine();
+            lengthName = name.Length + symbolEdgeName;
+
+            for (int i = 0; i < lengthName; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(symbol + name + symbol);
+
+            for (int i = 0; i < lengthName; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+    }
+}
